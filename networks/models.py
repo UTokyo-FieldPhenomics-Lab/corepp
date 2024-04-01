bugfix pce_large, added batch normalization in super3d and super3d_small, updated config files

networks/models.py
@@ -59,18 +59,23 @@ class EncoderPooled(nn.Module):
         # convolutions
         encoder = [
             nn.Conv2d(in_channels, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Flatten()
@@ -152,24 +157,31 @@ class EncoderBigPooled(nn.Module):
         # convolutions
         encoder = [
             nn.Conv2d(in_channels, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(1024),
             nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, True),
             nn.Flatten()
@@ -302,8 +314,8 @@ class PointCloudEncoderLarge(nn.Module):
         x = torch.max(x, 2, keepdim= True)[0]
         x = x.view(-1, 1024) # 1*1024
 
-        x = F.relu(self.bn6(self.fc1(x)))
-        x = F.relu(self.bn7(self.fc2(x)))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x 
     